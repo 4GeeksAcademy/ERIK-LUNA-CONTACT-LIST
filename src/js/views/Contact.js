9c8e8da0f1eb382fe22de5551fdd06c67b5ca4fd
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2024-01-18 15:39:08 +0000

    cambios

--- src/js/views/Contact.js
+++ src/js/views/Contact.js
@@ -10,36 +10,38 @@ import { Context } from "../store/appContext";
 export const Contacts = () => {
 	const { store, actions } = useContext(Context);
 	
-		return (
-			<div className="contactsPage container">
-				<Link to="/Add">
-					<button className="addContact btn btn-success">Add new contact</button>
-				</Link>
-						{store.contacts.map((item, index) => {
-							return (
-								<div className="contactGroup" key={index}>
-									<div className="contact">
-										<div className="image">
-										<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTue0QzcbncaPSxMCpLhzOab4a1Sux6xXpow&usqp=CAU"/>
-										</div>
-										<div className="information">
-											<div>
-												<h5>{item.full_name}</h5>
-												<p><FontAwesomeIcon icon={faLocationDot} /> {item.address}</p>
-												<p><FontAwesomeIcon icon={faPhone} /> {item.phone}</p>
-												<p><FontAwesomeIcon icon={faEnvelope} /> {item.email}</p>
-											</div>
-										</div>
-										<div className="editContact">
-											<Link to={"/Edit/"+ item.id}>
-											<button><FontAwesomeIcon icon={faPencil} className="pencil"/></button>
-											</Link>
-											<button onClick={() => actions.deleteContact(item.id)}><FontAwesomeIcon icon={faTrashCan} className="trash"/></button>
-										</div>
-									</div>
-								</div>
-									);
-								})}	
+	return (
+		<div className="contactsPage container">
+		  <div className="d-flex justify-content-center align-items-center vh-100">
+			<Link to="/Add">
+			  <button className="btn btn-primary w-70 max-w-200px mb-1">New contact</button>
+			</Link>
+		  </div>
+	  
+		  {store.contacts.map((item, index) => (
+			<div className="contactGroup mb-3" key={index}>
+			  <div className="contact">
+				<div className="image">
+				  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTue0QzcbncaPSxMCpLhzOab4a1Sux6xXpow&usqp=CAU" alt="Contact"/>
+				</div>
+				<div className="information">
+				  <div>
+					<h5>{item.full_name}</h5>
+					<p><FontAwesomeIcon icon={faLocationDot} /> {item.address}</p>
+					<p><FontAwesomeIcon icon={faPhone} /> {item.phone}</p>
+					<p><FontAwesomeIcon icon={faEnvelope} /> {item.email}</p>
+				  </div>
+				</div>
+				<div className="editContact">
+				  <Link to={"/Edit/"+ item.id}>
+					<button><FontAwesomeIcon icon={faPencil} className="pencil"/></button>
+				  </Link>
+				  <button onClick={() => actions.deleteContact(item.id)}>
+					<FontAwesomeIcon icon={faTrashCan} className="trash"/>
+				  </button>
+				</div>
+			  </div>
 			</div>
-	);
-	};
+		  ))}
+		</div>
+	  )};
