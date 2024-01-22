a8cb9f9895ace4290aad133dbb314d38ab00916a
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2024-01-22 22:55:01 +0000

    codigoModal

--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState, useEffect} from "react";
-import { Link, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "../../styles/AddContact.css";
 import { Context } from "../store/appContext";
 
@@ -7,14 +7,17 @@ export const AddContact = (props) => {
 
         const { store, actions } = useContext(Context);
         const params = useParams();
+        const navigate = useNavigate();
         const [fullName, setFullName] = useState("");
         const [phone, setPhone] = useState("");
         const [email, setEmail] = useState("");
         const [address, setAddress] = useState("");
         const newContact = {full_name: fullName, email: email, agenda_slug: "Erik-Luna", address: address, phone: phone };
-        function saveButton () {
+        function saveButton (e) {
+            e.preventDefault()
             if (props.opt == "add") {
-                return actions.createContact(newContact)
+                actions.createContact(newContact)
+                navigate("/")
             }
             if (props.opt == "edit") {
                 
@@ -29,7 +32,7 @@ export const AddContact = (props) => {
                       
                        fetch("https://playground.4geeks.com/apis/fake/contact/" + params.id, requestOptions)
                         .then(response => response.json())
-                        .then(result => console.log(result))
+                        .then(result => navigate("/"))
                 
             }
         }
@@ -46,29 +49,29 @@ export const AddContact = (props) => {
         }, [store.current_contact])
     
 	return (
-		<form action="/">
+		<form onSubmit={(e) => saveButton(e)}>
 			<h1 className="d-flex justify-content-center mt-5">{props.opt == "add" ? "Add a new contact" : "Edit contact"}</h1>
-			<div className="mb-3 mx-5">
+			<div className="mb-3 mx-3">
                 <label htmlFor="Name" className="form-label">Full Name</label>
-                <input value= {fullName} onChange={(e) => {setFullName(e.target.value)}} name="Name" type="text" className="form-control" placeholder="Full Name"/>
+                <input value= {fullName} onChange={(e) => {setFullName(e.target.value)}} name="Name" type="text" className="form-control" placeholder="Full Name" required/>
             </div>
-            <div className="mb-3 mx-5">
+            <div className="mb-3 mx-3">
                 <label htmlFor="Email" className="form-label">Email</label>
-                <input value= {email} onChange={(e) => {setEmail(e.target.value)}} name="Email" type="email" className="form-control" placeholder="Enter email" />
+                <input value= {email} onChange={(e) => {setEmail(e.target.value)}} name="Email" type="email" className="form-control" placeholder="Enter email" required/>
             </div>
-            <div className="mb-3 mx-5">
+            <div className="mb-3 mx-3">
                 <label htmlFor="Phone" className="form-label">Phone</label>
-                <input value= {phone} onChange={(e) => {setPhone(e.target.value)}} name="Phone" type="tel" className="form-control" placeholder="Enter phone" />
+                <input value= {phone} onChange={(e) => {setPhone(e.target.value)}} name="Phone" type="tel" className="form-control" placeholder="Enter phone" required />
             </div>
-			<div className="mb-3 mx-5">
+			<div className="mb-3 mx-3">
                 <label htmlFor="Address" className="form-label">Address</label>
-                <input value= {address} onChange={(e) => {setAddress(e.target.value)}} name="Address" type="tel" className="form-control" placeholder="Enter address" />
+                <input value= {address} onChange={(e) => {setAddress(e.target.value)}} name="Address" type="tel" className="form-control" placeholder="Enter address" required/>
             </div>
 			<div className="d-grid gap-2">
-                <button onClick={() => saveButton()} className="btn btn-primary mx-5 mt-3">Save</button>
-				<Link to="/" className="ms-5">
+                <button type="submit" className="btn btn-primary mx-5 mt-3">Save</button>
+				{/* <Link to="/" className="ms-5">
 					or get back to contacts
-				</Link>
+				</Link> */}
 			</div>
         </form>
 	);
--- src/js/views/Contact.js
+++ src/js/views/Contact.js
@@ -6,42 +6,64 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencil, faTrashCan, faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { Context } from "../store/appContext";
 
-
 export const Contacts = () => {
 	const { store, actions } = useContext(Context);
-	
+  
 	return (
-		<div className="contactsPage container">
-		  <div className="d-flex justify-content-center align-items-center vh-100">
-			<Link to="/Add">
-			  <button className="btn btn-primary w-70 max-w-200px mb-1">New contact</button>
-			</Link>
-		  </div>
-	  
-		  {store.contacts.map((item, index) => (
-			<div className="contactGroup mb-3" key={index}>
-			  <div className="contact">
-				<div className="image">
-				  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTue0QzcbncaPSxMCpLhzOab4a1Sux6xXpow&usqp=CAU" alt="Contact"/>
-				</div>
-				<div className="information">
-				  <div>
+	  <div className="contactsPage container">
+		<div className="d-flex justify-content-center align-items-center mb-3">
+		  <Link to="/Add">
+			<button className="btn btn-primary w-100 max-w-270px">New contact</button>
+		  </Link>
+		</div>
+  
+		<div className="row w-100">
+		  {store.contacts.map((item) => (
+			<div className="col-md-9" key={item.id}>
+			  <div className="contactGroup mb-3">
+				<div className="contact d-flex">
+				  <div className="image">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTue0QzcbncaPSxMCpLhzOab4a1Sux6xXpow&usqp=CAU" alt="Contact"/>
+				  </div>
+				  <div className="information ml-3 flex-grow-1">
 					<h5>{item.full_name}</h5>
 					<p><FontAwesomeIcon icon={faLocationDot} /> {item.address}</p>
 					<p><FontAwesomeIcon icon={faPhone} /> {item.phone}</p>
 					<p><FontAwesomeIcon icon={faEnvelope} /> {item.email}</p>
 				  </div>
-				</div>
-				<div className="editContact">
-				  <Link to={"/Edit/"+ item.id}>
-					<button><FontAwesomeIcon icon={faPencil} className="pencil"/></button>
-				  </Link>
-				  <button onClick={() => actions.deleteContact(item.id)}>
-					<FontAwesomeIcon icon={faTrashCan} className="trash"/>
-				  </button>
+				  <div className="editContact ml-3"> {/* Aquí añadí ml-3 para darle un poco de espacio a la izquierda */}
+					<Link to={"/Edit/"+ item.id}>
+					  <button className="btn btn-primary"><FontAwesomeIcon icon={faPencil} className="pencil"/></button>
+					</Link>
+					<button className="btn btn-primary" >
+					  <FontAwesomeIcon icon={faTrashCan} data-bs-toggle="modal" data-bs-target={"#deleteModal-"+item.id} className="trash"/>
+
+					  <div className="modal fade" id={"deleteModal-"+item.id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title text-dark" id={"deleteModalLabel-"+item.id}>Confirm Delete</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body text-dark">
+        Are you sure?
+      </div> 
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary" onClick={() => actions.deleteContact(item.id)} data-bs-dismiss="modal">Confirm</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+					</button>
+				  </div> 
 				</div>
 			  </div>
 			</div>
 		  ))}
 		</div>
-	  )};
+	  </div>
+	);
+  };
+  
